8b613428678755d9b7f969c8de52d6639736a49e
Thaley0 <pongsakon5219@gmail.com>
2026-02-20 15:44:29 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
         <tr>
             <td width="23" rowspan="2">&nbsp;</td>
             <td height="119" colspan="2" bgcolor="#006600">
-                <h1><center><font color="#FFFFFF">จิรวัฒนื อพาร์ตเมนต์</font></center></h1>
+                <h1><center><font color="#FFFFFF">จิรวัฒนื อพาร์ตเมนต์ดีดีดี</font></center></h1>
                 <?php include "Date.php"; ?>
             </td>
             <td width="43" rowspan="2">&nbsp;</td>
@@ -13,10 +13,10 @@
                 <img src="images/map.gif">
             </td>
             <td width="190" bgcolor="#FFFF66"><ul>
-				<li><a href="home.php">หน้าแรกฟฟฟฟฟฟฟ11111111111111111</li>
-				<li><a href="room.php">ห้องพัก&ราคาหหหหหห222222222222</li>
-				<li><a herf="guest.php">สมุดเยี่ยมหหหหห3333333333333</li>
-				<li><a href="contact.php">ติดต่หหหหอ33333333333</li>
+				<li><a href="home.php">หน้าแรกฟฟฟฟฟฟฟ11111111111zdfhxdf111111</li>
+				<li><a href="room.php">ห้องพัก&ราคาหหหหหห222222fdsgsfdg222222</li>
+				<li><a herf="guest.php">สมุดเยี่ยมหหหหห333333fghdfgfdg333333</li>
+				<li><a href="contact.php">ติดต่หหหหอ3333333333344fbfbgfd4444444</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
